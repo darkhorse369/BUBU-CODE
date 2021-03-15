e306b6005ee1f8b25c0b5c1c1ca7bd81fbfb40dc
JEDA <79952794+darkhorse369@users.noreply.github.com>
2021-03-15 14:01:48 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,57 +1,52 @@
 #include<stdio.h>
+
 #include<string.h>
+
 #include<stdlib.h>
 
 
+int main(int argc, char * argv[]) {
 
-int main(int argc, char *argv[]){
+  if (argc > 1) {
+    FILE * in = fopen(argv[1], "a+");
+    char buffer[2048] = "";
+    char str[2048] = "";
 
-if(argc>1){
-FILE *in=fopen(argv[1],"a+");
-char buffer[2048]="";
-char str[2048]="";
+    int i = 0;
 
-int i=0;
+    if ( in ) {
 
-if(in){
+      strcat(str, "echo \"\">");
+      strcat(str, argv[1]);
 
+      for (;;) {
 
-strcat(str,"echo \"\">");
-strcat(str,argv[1]);
+        fgets(buffer, 1024, in );
+        buffer[strlen(buffer) - 1] = 0;
 
-    for(;;){
+        if (strcmp(buffer, "clear") == 0) {
+          system("clear");
+        } else if (strcmp(buffer, "") == 0) {} else if (strcmp(buffer, "flush") == 0) {
+          system(str);
+          strcpy(buffer, "");
+        } else {
+          printf("%s\n", buffer);
+        }
 
-fgets(buffer,1024,in);
-buffer[strlen(buffer)-1]=0;
+        if (feof( in )) {
+          rewind( in );
+        }
 
-if(strcmp(buffer,"clear")==0){
-system("clear");
-}
-
-else if(strcmp(buffer,"")==0){}
-
-else if(strcmp(buffer,"flush")==0){system(str);strcpy(buffer,"");}
-
-else{
-    printf("%s\n",buffer);
-}
-
-if(feof(in)){
-rewind(in);
-}
-
-system(buffer);
-strcpy(buffer,"");
-strcpy(str,"");
+        system(buffer);
+        strcpy(buffer, "");
+        strcpy(str, "");
+      }
+      fclose( in );
+    } else {
+      printf("Error: File not found!\n");
     }
-fclose(in);
-}
 
-else{
-printf("Error: File not found!\n");
-}
-
-}
-    return 0;
+  }
+  return 0;
 
 }
